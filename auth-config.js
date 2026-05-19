4f39db71a80b416ee295cae2b045893946b6c956
pina753753-star <pina753753@gmail.com>
2026-05-19 09:53:21 +0900

    Add admin auth configuration placeholders

--- auth-config.js
+++ auth-config.js
@@ -1,5 +1,9 @@
 window.FLOWVID_AUTH = {
   supabaseUrl: "",
   supabaseAnonKey: "",
-  redirectTo: "https://pina753753-star.github.io/seedance-black-studio/profile.html"
+  redirectTo: "https://pina753753-star.github.io/seedance-black-studio/profile.html",
+  adminRedirectTo: "https://pina753753-star.github.io/seedance-black-studio/admin.html",
+  adminEmails: [
+    "hinaran53@gmail.com"
+  ]
 };
