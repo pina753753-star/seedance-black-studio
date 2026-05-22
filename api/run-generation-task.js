38731c5b3795fb561a14becf406a3129745c8483
pina753753-star <pina753753@gmail.com>
2026-05-23 03:54:26 +0900

    Remove disabled VEO run task API

--- api/run-generation-task.js
+++
@@ -1,9 +0,0 @@
-module.exports = async function handler(req, res) {
-  return res.status(200).json({
-    ok: true,
-    endpoint: '/api/run-generation-task',
-    disabled: true,
-    note: 'Temporarily disabled while Seedance integration is tested.',
-    checkedAt: new Date().toISOString()
-  });
-};
